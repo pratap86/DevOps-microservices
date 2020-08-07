936d451351b131a7e2fe5931cfc923fc29c1e660
pratap86 <narayanpratap86@gmail.com>
2020-08-07 18:47:54 +0530

    modified locations and notes

--- devops/flightservices/src/main/java/com/bharath/springcloud/repos/FlightRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bharath.springcloud.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bharath.springcloud.model.Flight;
-
-public interface FlightRepository extends JpaRepository<Flight, Long> {
-
-}
